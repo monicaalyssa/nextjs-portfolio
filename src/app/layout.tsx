161ea4f0af53409075b7fc6fb7850bbe4c9234e0
Monica Alyssa <monica_alyssa@icloud.com>
2024-12-31 20:20:53 -0500

    Add app layout margins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import '@mantine/core/styles.css';
-import { ColorSchemeScript, MantineProvider, createTheme, mantineHtmlProps } from '@mantine/core';
+import { ColorSchemeScript, Container, MantineProvider, createTheme, mantineHtmlProps } from '@mantine/core';
 
 const theme = createTheme({
   autoContrast: true,
@@ -26,10 +26,13 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript defaultColorScheme="dark"/>
         <link href="https://api.fontshare.com/v2/css?f[]=sentient@400,500,501,700,701&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, "/>
       </head>
       <body>
         <MantineProvider defaultColorScheme="dark" theme={theme}>
-          {children}
+          <Container>
+            {children}
+          </Container>
         </MantineProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 "use client"
-import styles from "./page.module.css";
-import { Button, Text, Title, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { Button, Title, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
 
 export default function Home() {
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true });
 
   return (
-    <div className={styles.page}>
-      <Title>Hello, I&apos;m Monica. 👋</Title>
+    <>
       <Button variant="default" onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}>Toggle</Button>
-      <Text>Hello Main Page</Text>
-      <Button>Button</Button>
-    </div>
+      <Title>Hello, I&apos;m Monica. 👋</Title>
+    </>
   );
 }
