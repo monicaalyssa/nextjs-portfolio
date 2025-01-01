b1f725ce2d2caad4ca9e7933b4113634d8114a93
Monica Alyssa <monica_alyssa@icloud.com>
2024-12-31 19:46:31 -0500

    Add light and dark mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,8 @@ import "./globals.css";
 import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider, createTheme, mantineHtmlProps } from '@mantine/core';
 
-const colorScheme: [string, string, string, string, string, string, string, string, string, string] = [ 
-    "#f5f5f5",
-    "#e7e7e7",
-    "#cdcdcd",
-    "#b2b2b2",
-    "#9a9a9a",
-    "#8b8b8b",
-    "#848484",
-    "#717171",
-    "#656565",
-    "#575757"
-]
-
 const theme = createTheme({
-  colors: {
-    colorScheme,
-  },
-  primaryColor: "colorScheme",
-  primaryShade: 9,
+  autoContrast: true,
 });
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import styles from "./page.module.css";
-import { Button, Text } from "@mantine/core";
+import { Button, Text, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
 
 export default function Home() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true });
+
   return (
     <div className={styles.page}>
+      <Button variant="default" onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}>Toggle</Button>
       <Text>Hello Main Page</Text>
       <Button>Button</Button>
     </div>
