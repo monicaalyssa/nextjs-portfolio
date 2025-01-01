c05c937a846e35058d1d4767c09d429c9c9170a3
Monica Alyssa <monica_alyssa@icloud.com>
2025-01-01 18:33:21 -0500

    Add tabs navigator

---
+++ src/app/main/tabs.tsx
@@ -0,0 +1,34 @@
+import { Flex, FloatingIndicator, UnstyledButton } from "@mantine/core";
+import { useState } from "react";
+import classes from "./tabs.module.css";
+
+const data = ["Education", "Work"];
+
+export default function Tabs() {
+  const [rootRef, setRootRef] = useState<HTMLDivElement | null>(null);
+  const [controlsRefs, setControlRefs] = useState<Record<string, HTMLButtonElement | null>>({});
+  const [active, setActive] = useState(0);
+
+  const setControlRef = (index: number) => (node: HTMLButtonElement) => {
+    controlsRefs[index] = node;
+    setControlRefs(controlsRefs);
+  };
+
+  const controls = data.map((item, index) => (
+    <UnstyledButton
+      ref={setControlRef(index)}
+      className={classes.control}
+      onClick={() => setActive(index)}
+      mod={{ active: active === index }}
+      key={item}
+    >
+      <span className={classes.controlLabel}>{item}</span>
+    </UnstyledButton>
+  ));
+  return (
+    <Flex mt="xl" className={classes.root} ref={setRootRef}>
+      {controls}
+      <FloatingIndicator className={classes.indicator} target={controlsRefs[active]} parent={rootRef} />
+    </Flex>
+  );
+}
