42efbdfde78e8a043bb6f549c7cd058d07f2e1cf
Monica Alyssa <monica_alyssa@icloud.com>
2025-01-01 15:06:04 -0500

    Update light and dark mode toggle icon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,18 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
 export default function About() {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+      setMounted(true);
+    }, []);
+  
+    if (!mounted) {
+      return null;
+    }
+
     return (
         <div>
             <p>Hello World</p>
--- src/app/main/navigation.tsx
+++ src/app/main/navigation.tsx
@@ -1,9 +1,13 @@
 "use client"
-import { Anchor, Flex, Button, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { Anchor, Flex, useComputedColorScheme, useMantineColorScheme, ActionIcon } from "@mantine/core";
+import { IconMoonStars, IconSun } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
     const { setColorScheme } = useMantineColorScheme();
+    const theme = useMantineColorScheme();
     const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true });
+    const [mounted, setMounted] = useState(false)
 
     const links = [
         {
@@ -27,10 +31,21 @@ export default function Navigation() {
             link: "/contact",
         },
     ]
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
     return (
         <>
         <Flex justify="space-between" align="center" my="xs">
-        <Button variant="default" onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}>Toggle</Button>
+        <ActionIcon onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')} variant="default">
+            {computedColorScheme === 'dark' ? <IconSun size={18}></IconSun> : <IconMoonStars size={18}></IconMoonStars>}
+        </ActionIcon>
         <Flex align="center" gap="xs">
             {links.map((link, key) => (
                 <Anchor size="sm" c="var(--text-color)" p="sm" fw={500} href={link.link} key={key}>{link.title}</Anchor>
