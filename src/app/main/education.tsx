e3a623e662f3c91d535e38aac600cf728932160a
Monica Alyssa <monica_alyssa@icloud.com>
2025-01-01 20:14:37 -0500

    Add work experience

--- src/app/main/education.tsx
+++ src/app/main/education.tsx
@@ -6,16 +6,16 @@ export default function Education() {
     return (
         <Timeline my="lg" color="var(--mantine-color-default)" active={1} bulletSize={50} lineWidth={2}>
             <Timeline.Item bullet={<Avatar size={51} 
-            src="https://yt3.googleusercontent.com/IRgDpgFolu8eHLU9ElcWkaY1TA7mNPGQUV05TCLRKJqEVm9HYggTiVeX42Dq1cIdH2v5LNNglg=s900-c-k-c0x00ffffff-no-rj" />} title="CareerFoundry">
+            src="/education_logo.png" />} title="CareerFoundry">
             <Text fw={500} size="sm">Full-Stack Development Certificate</Text>
             <Text c="dimmed" size="sm">Dec 2023 - Dec 2024</Text>
             <Text mt="xs" size="sm">Intensive, project-based online training program focused on front-end 
             and back-end web development. Coursework includes:</Text>
             
             <List my="xs" size="sm">
-                <ListItem>Frontend: JavaScript, React, HTML/CSS, React Native, Progressive Web Apps</ListItem>
-                <ListItem>Backend: Node.js, Express, REST APIs, OAuth, MongoDB, Firebase, PostgreSQL, JWT, AWS</ListItem>
-                <ListItem>Testing: Jest, Test-Driven Development, Puppeteer, Cucumber</ListItem>
+                <ListItem classNames={{itemWrapper: "listWrapper"}}>Frontend: JavaScript, React, HTML/CSS, React Native, Progressive Web Apps</ListItem>
+                <ListItem classNames={{itemWrapper: "listWrapper"}}>Backend: Node.js, Express, REST APIs, Authentication, MongoDB, Firebase, PostgreSQL, AWS</ListItem>
+                <ListItem classNames={{itemWrapper: "listWrapper"}}>Testing: Jest, Test-Driven Development, Puppeteer, Cucumber</ListItem>
             </List>
             <Link target="_blank" href="/Certificate.pdf">
             <Button aria-label="View CareerFoundry Certificate" variant="default" size="compact-xs">View Certificate</Button>
--- src/app/main/work.tsx
+++ src/app/main/work.tsx
@@ -1,7 +1,21 @@
-import { Text } from "@mantine/core";
+import { Avatar, List, ListItem, Text, Timeline } from "@mantine/core";
 
 export default function Work() {
+
     return (
-        <Text>Work</Text>
+        <Timeline my="lg" color="var(--mantine-color-default)" active={1} bulletSize={50} lineWidth={2}>
+            <Timeline.Item bullet={<Avatar size={51} 
+            src="/work_logo.png" />} title="Americas Mortgage Professionals">
+            <Text fw={500} size="sm">Web Developer and Marketing Designer</Text>
+            <Text c="dimmed" size="sm">Feb 2024 - Present</Text>
+            <Text mt="xs" size="sm"></Text>
+            
+            <List my="xs" size="sm">
+                <ListItem classNames={{itemWrapper: "listWrapper"}}>Design and develop a new, user-friendly, responsive company website with React, Vite, and TypeScript to better support the company&apos;s goals of expanding partnerships and attracting new talent.</ListItem>
+                <ListItem classNames={{itemWrapper: "listWrapper"}}>Create flyers for real estate agents to promote property listings and loan programs, strengthening realtor partnerships.</ListItem>
+            </List>
+
+            </Timeline.Item>
+        </Timeline>
     )
 }
