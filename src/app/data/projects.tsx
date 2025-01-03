6351ad95bff6f4b06a77cb363eb02cc8d8040fc8
Monica Alyssa <monica_alyssa@icloud.com>
2025-01-03 13:19:10 -0500

    Refactor projects into data folder

---
+++ src/app/data/projects.tsx
@@ -0,0 +1,36 @@
+export const projects = [
+    {
+      name: "PopcornPal",
+      favicon: "/popcornpal-logo.png",
+      description:
+        "PopcornPal is a full-stack web application that features a user-friendly platform that showcases detailed information about films and allows users to create accounts and favorite films.",
+      img: "/pp.png",
+      sourceCode: "/",
+      liveSite: "/",
+      techStack: ["React", "Node.js", "Express", "MongoDB"]
+    },
+    {
+      name: "Pokédex",
+      favicon: "https://monicaalyssa.github.io/pokedex/images/pokemon-icon.svg",
+      description: "A JavaScript web application that fetches data from a Pokémon API to display a list of Pokémon with images and details.",
+      img: "https://placehold.co/200x100",
+      sourceCode: "/",
+      liveSite: "/",
+      techStack: ["JavaScript", "RESTful API", "Bootstrap"]
+    },
+    {
+      name: "Mortgage Lender",
+      description:
+        "A local mortgage lender that offers various loan options and attracts potential leads and partners through interactive tools and resources.",
+      img: "https://placehold.co/200x100",
+      techStack: ["React", "Mantine", "Vite"]
+    },
+    {
+        name: "Meet",
+        description: "Meet is a Progressive Web App (PWA) with offline functionality that allows users to search for upcoming events near their city. It includes interactive charts to visualize key event data.",
+        img: "https://placehold.co/200x100",
+        sourceCode: "/",
+        liveSite: "/",
+        techStack: ["React", "AWS", "OAuth (Open Authorization)"]
+      },
+  ];
--- src/app/main/projects.tsx
+++ src/app/main/projects.tsx
@@ -1,48 +1,12 @@
-import { Text, Image, Grid, Button, Flex, useMantineColorScheme, Title, Box, Group } from "@mantine/core";
+import { Text, Image, Grid, Button, Flex, useMantineColorScheme, Title, Group } from "@mantine/core";
 import { IconArrowRight, IconBrandGithub, IconWorldWww } from "@tabler/icons-react";
+import { projects } from "../data/projects";
 
 export default function Projects() {
   // dim hover & scale
   // whole button will link to separate page, but inside will have 2 buttons
 
   const theme = useMantineColorScheme();
-
-  const projects = [
-    {
-      name: "PopcornPal",
-      favicon: "/popcornpal-logo.png",
-      description:
-        "PopcornPal is a full-stack web application that features a user-friendly platform that showcases detailed information about films and allows users to create accounts and favorite films.",
-      img: "/pp.png",
-      sourceCode: "/",
-      liveSite: "/",
-      techStack: ["React", "Node.js", "Express", "MongoDB"]
-    },
-    {
-      name: "Pokédex",
-      favicon: "https://monicaalyssa.github.io/pokedex/images/pokemon-icon.svg",
-      description: "A JavaScript web application that fetches data from a Pokémon API to display a list of Pokémon with images and details.",
-      img: "https://placehold.co/200x100",
-      sourceCode: "/",
-      liveSite: "/",
-      techStack: ["JavaScript", "RESTful API", "Bootstrap"]
-    },
-    {
-      name: "Mortgage Lender",
-      description:
-        "A local mortgage lender that offers various loan options and attracts potential leads and partners through interactive tools and resources.",
-      img: "https://placehold.co/200x100",
-      techStack: ["React", "Mantine", "Vite"]
-    },
-    {
-        name: "Meet",
-        description: "Meet is a Progressive Web App (PWA) with offline functionality that allows users to search for upcoming events near their city. It includes interactive charts to visualize key event data.",
-        img: "https://placehold.co/200x100",
-        sourceCode: "/",
-        liveSite: "/",
-        techStack: ["React", "AWS", "OAuth (Open Authorization)"]
-      },
-  ];
 
   return (
     <Group py="xs" my="xl">
